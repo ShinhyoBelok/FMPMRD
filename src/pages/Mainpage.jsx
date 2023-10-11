d08cb4f79584c5e865cab15e944be0ea42c69a5e
ShinhyoBelok <shibo6937@gmail.com>
2023-10-11 14:54:29 -0400

    Add all html elements

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BsFacebook, BsYoutube } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai';
 
 export default function Mainpage() {
   return (
@@ -14,13 +16,101 @@ export default function Mainpage() {
           <li><a href="#">Contact Us</a></li>
         </ul>
       </nav>
-      <div className="socials">Socials and donations</div>
-      <section className="hero">Hero</section>
-      <section className="about_us">About info section</section>
-      <section className="stories">Stories</section>
-      <section className="areas_of_focus">Area de focus</section>
-      <section className="take_action_donation">Action donation</section>
-      <footer>Footer</footer>
+      <div className="socials">
+        <div className="icons">
+          <BsFacebook />
+          <AiFillInstagram />
+          <BsYoutube />
+        </div>
+        <button>Donate</button>
+      </div>
+      <section className="hero">
+        <img src="src\assets\hero1.jpg" alt="Hero pic" />
+      </section>
+      <section className="about_us">
+        <article>
+          <img src="src\assets\boxesimg-about-us.jpg" alt="picture of smiling girl" />
+          <h2>ABOUT US</h2>
+          <p>Women working together to realize one global family rooted in a culture of sustainable peace.</p>
+        </article>
+        <article>
+          <img src="src\assets\boxesimg-about-us.jpg" alt="picture of smiling girl" />
+          <h2>WHAT WE DO</h2>
+          <p>Women working together to realize one global family rooted in a culture of sustainable peace.</p>
+        </article>
+        <article>
+          <img src="src\assets\boxesimg-about-us.jpg" alt="picture of smiling girl" />
+          <h2>UPCOMING EVENTS</h2>
+          <p>Women working together to realize one global family rooted in a culture of sustainable peace.</p>
+        </article>
+        <article>
+          <img src="src\assets\boxesimg-about-us.jpg" alt="picture of smiling girl" />
+          <h2>UNITED NATIONS OFFICES</h2>
+          <p>Women working together to realize one global family rooted in a culture of sustainable peace.</p>
+        </article>
+      </section>
+      <section className="stories">
+        <div className="story_pic">
+          <img src="src\assets\storie1.jpg" alt="storie pic" />
+        </div>
+        <div className="story_desc">
+          <h2>SUCCESS STORIES:</h2>
+          <p>
+            The Girl Child Empowerment Project in WFWP Burkina Faso 
+            WFWP Burkina Faso organized a workshop in Lycée Bogodogo 
+            with students from the Ouagadougou International School 
+            in six different cities and schools. Each workshop was 
+            attended by 50 to 250 children.
+          </p>
+          <button>READ MORE</button>
+        </div>
+      </section>
+      <section className="areas_of_focus">
+        <div className="focus">
+          <img src="src\assets\areaoffocus1.png" alt="world" />
+          <h4>PROYECTOS HUMANITARIOS</h4>
+        </div>
+        <div className="focus">
+          <img src="src\assets\areaoffocus1.png" alt="world" />
+          <h4>PROYECTOS HUMANITARIOS</h4>
+        </div>
+        <div className="focus">
+          <img src="src\assets\areaoffocus1.png" alt="world" />
+          <h4>PROYECTOS HUMANITARIOS</h4>
+        </div>
+        <div className="focus">
+          <img src="src\assets\areaoffocus1.png" alt="world" />
+          <h4>PROYECTOS HUMANITARIOS</h4>
+        </div>
+        <div className="focus">
+          <img src="src\assets\areaoffocus1.png" alt="world" />
+          <h4>PROYECTOS HUMANITARIOS</h4>
+        </div>
+        <div className="focus">
+          <img src="src\assets\areaoffocus1.png" alt="world" />
+          <h4>PROYECTOS HUMANITARIOS</h4>
+        </div>
+      </section>
+      <section className="take_action_donation">
+        <img src="src\assets\takeaction.jpg" alt="kid" />
+        <div className="action">
+          <h3>TOMAR ACCIÓN</h3>
+          <p>
+            Learn more about how you can donate or help raise 
+            money for children, students and education programs 
+            in need of a helping hand.
+          </p>
+          <button>DONAR</button>
+        </div>
+      </section>
+      <footer>
+        <div className="icons">
+          <BsFacebook />
+          <AiFillInstagram />
+          <BsYoutube />
+        </div>
+        <p>COPYRIGHT &copy; WOMEN'S FEDERATION FOR WORLD PEACE, INTERNATIONAL - ALL RIGHTS RESERVED  |  4 W 43RD STREET, NEW WORK, NY 10036, USA</p>
+      </footer>
     </div>
   )
 }
